ede5c2c3113c5ea5feaf159465e3101091f85af0
khe4oyan <khe4oyan@gmail.com>
2024-05-21 20:07:13 +0400

    .

--- pages/accept/accept.js
+++ pages/accept/accept.js
@@ -1,32 +1,80 @@
 import DOM from "../../tools/dom.js";
 import API from "../../tools/api.js";
 
-const dateDOM = DOM.GE('.date');
-const nameDOM = DOM.GE('.name');
-const surnameDOM = DOM.GE('.surname');
-const roomsDOM = DOM.GE('.rooms');
-const emailDOM = DOM.GE('.email');
-const phoneDOM = DOM.GE('.phone');
+// DOM elements
+const dateDOM = DOM.GE(".date");
+const nameDOM = DOM.GE(".name");
+const surnameDOM = DOM.GE(".surname");
+const roomsDOM = DOM.GE(".rooms");
+const emailDOM = DOM.GE(".email");
+const phoneDOM = DOM.GE(".phone");
+const registerButtonDOM = DOM.GE(".registerButton");
 
 // set minimal day for choose
-const today = new Date().toISOString().split('T')[0];
+const today = new Date().toISOString().split("T")[0];
 dateDOM.min = today;
 
-dateDOM.addEventListener('change', (e) => {
-  API.checkFreeDays(e.target.value)
-  .then(d => {
+// event listeners
+dateDOM.addEventListener("change", (e) => {
+  roomsDOM.innerHTML = "";
+  roomsDOM.value = null;
+  API.checkFreeDays(e.target.value).then((d) => {
     if (d?.data) {
-      roomsRender(d.data, rooms);
+      roomsRender(d.data);
+      registerButtonActivateCheck();
     }
   });
 });
 
-function roomsRender(rooms, roomsDOM) {
+roomsDOM.addEventListener("change", (e) => {
+  registerButtonActivateCheck();
+});
+
+nameDOM.addEventListener("input", (e) => {
+  registerButtonActivateCheck();
+});
+surnameDOM.addEventListener("input", (e) => {
+  registerButtonActivateCheck();
+});
+emailDOM.addEventListener("input", (e) => {
+  registerButtonActivateCheck();
+});
+phoneDOM.addEventListener("input", (e) => {
+  registerButtonActivateCheck();
+});
+
+registerButtonDOM.addEventListener("click", () => {
+  // TODO
+});
+
+// =======================
+// FUNCTIONS ==================
+function registerButtonActivateCheck() {
+  if (
+    dateDOM.value &&
+    nameDOM.value &&
+    surnameDOM.value &&
+    roomsDOM.value &&
+    emailDOM.value &&
+    phoneDOM.value
+  ) {
+    registerButtonDOM.disabled = false;
+  } else {
+    registerButtonDOM.disabled = true;
+  }
+}
+
+function roomsRender(rooms) {
   for (let i = 0; i < rooms.length; ++i) {
-    const option = DOM.CE('option');
+    const option = DOM.CE("option");
     option.value = rooms[i];
     option.innerHTML = rooms[i];
 
     roomsDOM.appendChild(option);
+  }
+
+
+  if (rooms.length > 0) {
+    roomsDOM.disabled = false;
   }
 }
